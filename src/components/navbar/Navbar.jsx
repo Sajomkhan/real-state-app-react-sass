48aea0599228147490d9cf3878ec6357ce2cfa06
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-04-10 00:25:34 +0600

    Navebar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,38 @@
-import "./navbar.scss"
+import { useState } from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
-      <div className="left">Left</div>
-      <div className="right">Right</div>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>RealEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sing in</a>
+        <a href="/" className="register">
+          Register
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={() => setOpen(!open)} />
+        </div>
+        <div className={open? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Register</a>
+        </div>
+      </div>
     </nav>
   );
 };
